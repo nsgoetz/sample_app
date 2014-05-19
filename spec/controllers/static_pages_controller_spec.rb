f58693edb31adfc6f327175d43b00aa06d860af3
nsgoetz <Noah Goetz>
2014-05-19 16:48:26 -0700

    try 2

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
-  describe "GET '...'" do
+  describe "GET 'home'" do
     it "returns http success" do
-      get '...'
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
       response.should be_success
     end
   end
